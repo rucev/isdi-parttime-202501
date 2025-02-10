7dfc7dfeed98111cc787cfbef6e766b1e8466585
Flors Rueda <ruedaflors@gmail.com>
2025-02-10 19:55:12 +0100

    add for vs while #19

---
+++ staff/flors-rueda/stuff/whileVSfor/index.js
@@ -0,0 +1,12 @@
+var arr = [1, 2, 3, 4, 5]
+
+for (var index = 0; index < arr.length; index++) {
+    console.log(arr[index])
+}
+
+
+var jndex = 0;
+while (jndex < arr.length) {
+    console.log(arr[jndex])
+    jndex++
+}
