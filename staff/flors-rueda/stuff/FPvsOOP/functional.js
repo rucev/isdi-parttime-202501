502094e18b6866f64c6b37221e2a42400870464b
Flors Rueda <ruedaflors@gmail.com>
2025-03-17 20:29:05 +0100

    add example of oop vs fp #19

--- staff/flors-rueda/stuff/FPvsOOP/functional.js
+++ staff/flors-rueda/stuff/FPvsOOP/functional.js
@@ -3,3 +3,29 @@
 - Prioriza datos inmutables.
 - En lugar de modificar el objeto, se devuelve uno nuevo con los cambios.
 */
+
+const createNewUser = (username, email, password, avatar) => {
+    const user = {
+        username,
+        email,
+        password,
+        avatar,
+        id: Date.now()
+    }
+
+    return user
+}
+
+const updateUsername = (newUsername, user) => {
+    user.username = newUsername
+    return user
+}
+
+let user1 = createNewUser('pepito', 'pepito@mail.com', '12345!!ASDNks', 'link.png')
+
+console.log(user1.username, user1.id)
+
+user1 = updateUsername('Juan', user1)
+
+console.log(user1.username)
+
--- staff/flors-rueda/stuff/FPvsOOP/object-orientated.js
+++ staff/flors-rueda/stuff/FPvsOOP/object-orientated.js
@@ -2,3 +2,29 @@
 - Se usan clases y objetos con métodos.
 - Se modifica el estado interno del objeto usando esos métodos.
 */
+
+class User {
+    constructor(username, email, password, avatar) {
+        this.username = username
+        this.email = email
+        this.password = password
+        this.avatar = avatar
+        this.id = Date.now()
+    }
+
+    updateUsername = (newUsername) => {
+        this.username = newUsername
+    }
+
+    updateEmail = (newEmail) => {
+        this.newEmail = newEmail
+    }
+}
+
+const user1 = new User('pepito', 'pepito@mail.com', '12345!!ASDNks', 'link.png')
+
+console.log(user1.username, user1.id)
+
+user1.updateUsername('Juan')
+
+console.log(user1.username)
