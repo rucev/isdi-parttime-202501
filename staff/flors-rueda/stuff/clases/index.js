a9445ed410a778ef720ccf4209017f1a8f017dc7
Flors Rueda <ruedaflors@gmail.com>
2025-03-17 20:12:44 +0100

    add clases demo #19

--- staff/flors-rueda/stuff/clases/index.js
+++ staff/flors-rueda/stuff/clases/index.js
@@ -3,9 +3,96 @@ Una clase es una forma de crear objetos con un conjunto de propiedades y método
 Se introdujeron en ES6 (ECMAScript 2015) como una forma más organizada de trabajar con objetos y herencia.
 */
 
-//Definir una clase persona con "name" y "age" como propiedades
-//que tenga un metodo: "sayName", y otro "sayAge"
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age
+    }
 
-//crear varias instancias de persona
+    sayName = () => {
+        console.log(`Hi, my name is ${this.name}`)
+    }
 
-//crear una clase (estudiante) que hereda las propiedades de persona y añade más
+    sayAge = () => {
+        console.log(`I'm ${this.age} years old`)
+    }
+}
+
+
+class Person extends Animal {
+    constructor(name, age, shoeSize) {
+        super(name, age)
+        this.shoeSize = shoeSize
+    }
+
+    sayShoeSize = () => {
+        console.log(`I'm ${this.name}. My shoe size is ${this.shoeSize}`)
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, age, breed, isNeutered) {
+        super(name, age)
+        this.breed = breed;
+        this.isNeutered = isNeutered
+    }
+
+    sayDogInfo = () => {
+        console.log(`${this.name}, is a ${this.breed} who ${this.isNeutered ? 'is neutered' : 'is not neutered'}.`)
+    }
+}
+
+class SleepyDog extends Dog {
+    constructor(name, age, breed, isNeutered, hoursOfSleep) {
+        super(name, age, breed, isNeutered)
+        this.hoursOfSleep = hoursOfSleep
+    }
+
+    sleepInfo = () => {
+        console.log(`${this.name} sleeps ${this.hoursOfSleep} hours everyday`)
+    }
+
+    updateSleepHours = (newHours) => {
+        this.hoursOfSleep = newHours
+    }
+}
+
+/*
+const flors = new Person('Flors', 29, 40)
+
+const percy = new Dog('Percy', 4, 'mix', true)
+
+flors.sayName()
+flors.sayShoeSize()
+
+percy.sayName()
+percy.sayDogInfo()
+*/
+
+
+const percy = new SleepyDog('Percy', 4, 'mix', true, 17)
+
+percy.sleepInfo()
+
+percy.updateSleepHours(20)
+
+percy.sleepInfo()
+
+console.log(percy.name)
+
+//
+
+class Client extends Person {
+    constructor(name, age, shoeSize, monthlyExpenses) {
+        super(name, age, shoeSize)
+        this.monthlyExpenses = monthlyExpenses
+    }
+}
+
+const client1 = new Client('Juan', 34, 40, 4000)
+const client2 = new Client('Flors', 29, 40, 5000)
+const client3 = new Client('Laura', 30, 40, 9999)
+
+const clients = [client1, client2, client3]
+
+console.log(clients.filter(client => client.monthlyExpenses >= 4500))
