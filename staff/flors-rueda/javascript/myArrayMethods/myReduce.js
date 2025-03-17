44173f977e0e3cedd99f144e63f9e8ec01357ba3
Flors Rueda <ruedaflors@gmail.com>
2025-03-17 21:29:23 +0100

    add myReduce #1

---
+++ staff/flors-rueda/javascript/myArrayMethods/myReduce.js
@@ -0,0 +1,40 @@
+const arrayControl = ['ant', 'bison', 'camel', 'duck', 'elephant'] //'ant, bison, camel and more liked this post'
+const arrayTest = ['ant', 'bison', 'camel', 'duck', 'elephant']
+
+const concatWords = (accumulator, currentValue, currentIndex, array) => {
+    if (currentIndex <= 2) {
+        return accumulator + ', ' + currentValue
+    } else if (currentIndex === 3) {
+        return accumulator + ' and more'
+    } else {
+        return accumulator
+    }
+}
+
+const myReduce = (array, callback, initialValue) => {
+    let result = initialValue
+
+    for (let i = 0; i < array.length; i++) {
+        let element = callback(result, array[i], i, array)
+        result = element
+    }
+
+    return result
+}
+
+
+console.log('testing array.reduce()');
+
+let initialValue = ''
+let controlResult1 = arrayControl.reduce((element, index, array) => concatWords(element, index, array), initialValue)
+let testResult1 = myReduce(arrayTest, concatWords, initialValue)
+
+
+for (let i = 0; i < arrayControl.length; i++) {
+    console.assert(arrayTest[i] === arrayControl[i], `index ${i} is diferent in both arrays. ${arrayTest[i]} !== ${arrayControl[i]}`);
+}
+
+let lengthToTest = controlResult1.length > testResult1.length ? controlResult1.length : testResult1.length;
+for (let i = 0; i < lengthToTest; i++) {
+    console.assert(testResult1[i] === controlResult1[i], `index ${i} is diferent in both arrays. ${testResult1[i]} !== ${controlResult1[i]}`);
+}
