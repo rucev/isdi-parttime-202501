4a10e79e778e18c55d9a7f358d729edc28fc9af8
Flors Rueda <ruedaflors@gmail.com>
2025-03-19 21:18:42 +0100

    delete old react folder #19

--- staff/flors-rueda/stuff/react-oop/react/main.jsx
+++
@@ -1,6 +0,0 @@
-const reactElement = React.createElement;
-
-const mainContainer = document.getElementById('app');
-const root = ReactDOM.createRoot(mainContainer);
-
-root.render(reactElement(App));
