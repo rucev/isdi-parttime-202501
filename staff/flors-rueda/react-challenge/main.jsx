e5de6a4c5f99efa20434d4de0590102cf610ce7c
Flors Rueda <ruedaflors@gmail.com>
2025-03-20 18:01:31 +0100

    clean react challenge files #1

--- staff/flors-rueda/react-challenge/main.jsx
+++ staff/flors-rueda/react-challenge/main.jsx
@@ -1,5 +1,4 @@
 const reactElement = React.createElement;
-if (!localStorage.record) localStorage.record = JSON.stringify([])
 
 const mainContainer = document.getElementById('app');
 const root = ReactDOM.createRoot(mainContainer);
--- staff/flors-rueda/react-challenge/solved/src/components/Btn.jsx
+++
@@ -1,5 +0,0 @@
-/*Este componente cumple un proposito muy parecido al de la función de createButton que tenemos hecha en nuestra app de js vanilla*/
-
-const Btn = ({ btnContent, btnCallback, className }) => {
-    return <button className={className} onClick={btnCallback}>{btnContent}</button>
-}
