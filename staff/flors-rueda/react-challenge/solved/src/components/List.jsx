14cd7c083dad2df8eb8e0e3a2c741c558a695db0
Flors Rueda <ruedaflors@gmail.com>
2025-03-20 17:37:10 +0100

    add challenge solution #1

---
+++ staff/flors-rueda/react-challenge/solved/src/components/List.jsx
@@ -0,0 +1,24 @@
+const List = ({ items, onItemClick }) => {
+    console.log(items)
+    return <div>
+        {items.length > 0 ?
+            <ol>
+                {items.map((item, index) => {
+                    return <li key={index}>
+                        <div className="list-item">
+                            {item}
+                            <Btn
+                                className={'delete'}
+                                btnCallback={() => onItemClick(index)}
+                                btnContent={'X'}
+                            />
+                        </div>
+                    </li>
+                })}
+            </ol>
+            :
+            <p>No hay datos todavía</p>
+        }
+
+    </div>
+}
